8168dda411d45776195570f9479cb5e17e2cb86b
Jaideep <jaideep@tokbox.com>
2015-11-20 19:03:39 -0800

    POC

---
+++ Hello-World/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Hello-World
+//
+//  Copyright (c) 2013 TokBox, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *justALabel;
+
+@end
